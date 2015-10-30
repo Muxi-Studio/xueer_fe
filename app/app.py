e7cc773686615108cc8a6841a48585005c5b708e
zxc0328 <120797887@qq.com>
2015-10-30 21:38:26 +0800

    update

--- app/app.py
+++ app/app.py
@@ -7,9 +7,9 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/test')
-def test():
-    return render_template("test.html")
+@app.route('/')
+def index():
+    return render_template("pages/index.html")
 
 
 if __name__ == "__main__":
