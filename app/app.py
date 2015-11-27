797a1f0281fb048ce9d0f4692692b5ff09da1358
zxc0328 <120797887@qq.com>
2015-11-27 19:39:28 +0800

    update, to run on server

--- app/app.py
+++ app/app.py
@@ -10,18 +10,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	file = open('mock/index.json', 'r')
-	json_dict = json.loads(file.read())
-	file.close()
-	return render_template("pages/index.html",top_list=json_dict['top_list'])
+    with app.open_resource('mock/index.json') as f:
+        data = f.read()
+        json_dict = json.loads(data)
+    return render_template("pages/index.html", top_list=json_dict['top_list'])
 
 
 @app.route('/course')
 def course():
-	file = open('mock/course.json', 'r')
-	json_dict = json.loads(file.read())
-	file.close()
-	return render_template("pages/course.html",info=json_dict['info'],hot_comments=json_dict['hot_comments'])
+    with app.open_resource('mock/course.json') as f:
+        data = f.read()
+        json_dict = json.loads(data)
+    return render_template("pages/course.html",info=json_dict['info'],hot_comments=json_dict['hot_comments'])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/src/mock_template/index.js
+++ app/src/mock_template/index.js
@@ -1,24 +1,14 @@
 exports.obj = {
-    "top_list|3": {
-        '0|6': [{
-            'name|1-3':"心理学",
-            'url': "#"
-        }],
-        '1|6': [{
-            'name': "变态心理学",
-            'url': "#"
-        }]
-        ,
-        '2|6': [{
-            'name': "变态心理学",
-            'url': "#"
-        }]
-    },
-    "recommend|6":[
+    "top_list|10": [{
+        'title':'毛泽东思想与中国特色社会主义理论体系概论',
+        'teacher':'王大锤',
+        'url':'/course/1',
+        'likes':30
+    }],
+    "banner|3":[
     	{
-    		'name|1-3':"心理学",
-    		'teacher':"朱承浩",
-            'url': "#"
+    		'url':"tips/1",
+            'img_url': "http://foo.com/a.png"
     	}
     ]
 }
